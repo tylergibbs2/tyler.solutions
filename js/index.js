f072f5a03d3ffbd1fea949a28b3bb51bcfbd4608
Tyler <gibbstyler7@gmail.com>
2022-11-25 21:42:07 -0600

    refactors index page

---
+++ js/index.js
@@ -0,0 +1,40 @@
+var seenIndices = [];
+let subtitles = [
+    "Pioneer of writing subtitles",
+    "Creator of Coyote/Mouse Simulation",
+    "Ex-user of jQuery 😔",
+    "<i>is feeling great!</i>",
+    "Software Engineer",
+    "Backend Engineer",
+    "Frontend Engineer",
+    "Definitely doesn't make floating-point errors",
+    "Pythonista",
+    "React > Angular",
+    "<i>undefined</i>",
+    "Stardew Valley modder"
+];
+
+function randomizeSubtitle() {
+    let subtitle = document.getElementById("description");
+
+    if (seenIndices.length === subtitles.length)
+        seenIndices.length = 0;
+
+    let randomIdx = Math.floor(Math.random() * subtitles.length);;
+    while (seenIndices.includes(randomIdx))
+        randomIdx = Math.floor(Math.random() * subtitles.length);
+    seenIndices.push(randomIdx);
+
+    subtitle.innerHTML = subtitles[randomIdx];
+}
+
+function randomizeBackgroundColor() {
+    document.body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+window.onload = function () {
+    document.getElementById("profilePic").onclick = randomizeBackgroundColor;
+    document.getElementById("description").onclick = randomizeSubtitle;
+
+    randomizeSubtitle();
+}
