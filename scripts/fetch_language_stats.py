3a383fb7bfb0f5eab3ff436427da5f6ab53186a2
Tyler Gibbs <gibbstyler7@gmail.com>
2024-07-26 19:59:36 -0500

    add ability to get language color

--- scripts/fetch_language_stats.py
+++ scripts/fetch_language_stats.py
@@ -8,6 +8,7 @@ GITHUB_USERNAME = "tylergibbs2"
 TARGET_FILENAME = "language-stats.json"
 REPOS_URL = "https://api.github.com/users/{username}/repos?per_page=100"
 LANGUAGES_URL = "https://api.github.com/repos/{username}/{repo_name}/languages"
+COLORS_URL = "https://raw.githubusercontent.com/ozh/github-colors/master/colors.json"
 
 
 def get_language_stats(username: str, repo_name: str) -> dict:
@@ -15,6 +16,11 @@ def get_language_stats(username: str, repo_name: str) -> dict:
         username=username,
         repo_name=repo_name
     ))
+    return resp.json()
+
+
+def get_language_colors() -> dict:
+    resp = requests.get(COLORS_URL)
     return resp.json()
 
 
@@ -37,6 +43,11 @@ def main() -> None:
     total = sum(global_stats.values())
     result = [{"language": key, "percentage": (value / total) * 100} for key, value in global_stats.items()]
     result = sorted(result, key=lambda r: r["percentage"], reverse=True)
+
+    colors_map = get_language_colors()
+    for language in result:
+        language["color"] = colors_map.get(language["language"], "#fff")
+
     with open(TARGET_FILENAME, "w") as fd:
         json.dump(result, fd)
 
